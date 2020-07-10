5742edbc9b18fc1680c30d99f5918b4e0112fbd8
Lilia Solovei <63199181+l-solovei@users.noreply.github.com>
2020-07-11 01:52:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -185,37 +185,37 @@
 //		el_car.append(div_car)
 //	}
 //}
-const arrcar = [
-{
-	model : 'BMW',
-	year : 1967,
-	owner : true,
-	statusnow : 'sale',
-	color : 'black'},
-{
-	model : 'Volvo',
-	year : 1998,
-	owner : true,
-	statusnow : 'rent',
-	color : 'red'},
-{
-	model : 'Opel',
-	year : 2010,
-	owner : false,
-	statusnow : 'sale',
-	color : 'white'},
-{
-	model : 'Mustang',
-	year : 1955,
-	owner : false,
-	statusnow : 'svalka',
-	color : 'green'},
-{
-	model : 'MiniCuper',
-	year : 2017,
-	owner : true,
-	statusnow : 'uses',
-	color : 'yellow'}];
+//const arrcar = [
+//{
+//	model : 'BMW',
+//	year : 1967,
+//	owner : true,
+//	statusnow : 'sale',
+//	color : 'black'},
+//{
+//	model : 'Volvo',
+//	year : 1998,
+//	owner : true,
+//	statusnow : 'rent',
+//	color : 'red'},
+//{
+//	model : 'Opel',
+//	year : 2010,
+//	owner : false,
+//	statusnow : 'sale',
+//	color : 'white'},
+//{
+//	model : 'Mustang',
+//	year : 1955,
+//	owner : false,
+//	statusnow : 'svalka',
+//	color : 'green'},
+//{
+//	model : 'MiniCuper',
+//	year : 2017,
+//	owner : true,
+//	statusnow : 'uses',
+//	color : 'yellow'}];
 //car_el(arrcar, 'p');
 
 // DONE приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
@@ -237,15 +237,107 @@ const arrcar = [
 //function car_el3(car_arr, el_id){
 //	for (let i of car_arr){
 //		for (let j in i){
-//		car_prop = `${j} : ${i[j]}`;
-//		const div_prop = document.createElement('div');
-//		const div_car = document.createElement('div');
-//		const el_car = document.createElement('el_id');
-//		div_prop.innerText = car_prop;
-//		document.body.append(el_car);
-//		el_car.append(div_car);
-//		div_car.append(car_prop);
+//			car_prop = `${j} : ${i[j]}`;
+//			const div_prop = document.createElement('div');
+//			const div_car = document.createElement('div');
+//			const el_car = document.createElement('el_id');
+//			div_prop.innerText = car_prop;
+//			document.body.append(el_car);
+//			el_car.append(div_car);
+//			div_car.append(car_prop);
 //		}
 //	}
-}
-car_el3(arrcar, 'p');
+//}
+//car_el3(arrcar, 'p');
+
+
+
+
+
+//(на основі минулого ДЗ)
+//**-DONE функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
+//та повертає масив цих з'єднаних об'єктів.
+//Приклад масивів:
+//let usersWithId = [
+//	{id: 1, name: 'vasya', age: 31, status: false},
+//	{id: 2, name: 'petya', age: 30, status: true},
+//	{id: 3, name: 'kolya', age: 29, status: true},
+//	{id: 4, name: 'olya', age: 28, status: false},];
+//let citiesWithId = [
+//	{user_id: 3, country: 'USA', city: 'Portland'},
+//	{user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//	{user_id: 2, country: 'Poland', city: 'Krakow'},
+//	{user_id: 4, country: 'USA', city: 'Miami'},];
+
+//function user_city(usersWithId, citiesWithId){
+//	for (i of usersWithId){
+//		for (j of citiesWithId){
+//			if (i.id === j.user_id){
+//				i.adress = j;
+//			}
+//		}
+//	}
+//	return usersWithId;
+//}
+//console.log(user_city(usersWithId, citiesWithId));
+
+
+
+
+
+//DONE ***- беремо завдання з правилами з укроку №3 :
+//Та робимо це функцією.При цьому правила отримувати через аргумент.
+//"Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+//При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+//Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
+
+//let rules = [
+//	{
+//		title: 'Первое правило Бойцовского клуба.',
+//		body: 'Никому не рассказывать о Бойцовском клубе.'
+//	},
+//	{
+//		title: 'Второе правило Бойцовского клуба.',
+//		body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//	},
+//	{
+//		title: 'Третье правило Бойцовского клуба.',
+//		body: 'В схватке участвуют только двое.'
+//	},
+//	{
+//		title: 'Четвертое правило Бойцовского клуба.',
+//		body: 'Не более одного поединка за один раз.'
+//	},
+//	{
+//		title: 'Пятое правило Бойцовского клуба.',
+//		body: 'Бойцы сражаются без обуви и голые по пояс.'
+//	},
+//	{
+//		title: 'Шестое правило Бойцовского клуба.',
+//		body: 'Поединок продолжается столько, сколько потребуется.'
+//	},
+//	{
+//		title: 'Седьмое правило Бойцовского клуба.',
+//		body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//	},
+//	{
+//		title: 'Восьмое и последнее правило Бойцовского клуба.',
+//		body: 'Новичок обязан принять бой.'
+//	},
+//];
+
+//function f_rules(rules){
+//	for (i of rules){
+//		let key = document.createElement('div');
+//		key.innerText = i.title;
+//		key.style.fontSize = '30px';
+//		key.style.fontWeight = 'bold';
+//		key.style.padding = '10px';
+//		let value = document.createElement('div');
+//		value.innerText = i.body;
+//		value.style.padding = '10px';
+//		document.body.append(key);
+//		document.body.append(value);
+//	}
+//}
+//f_rules(rules);
